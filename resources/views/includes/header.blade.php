4e79f3d059cc5383df847128c0427abd14623447
Chris <stamlercas@yahoo.com>
2016-09-19 01:41:33 -0400

    Edited header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -8,7 +8,7 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-              <a class="navbar-brand" href="{{ route('dashboard') }}">The Wolfe<sub> Beta</sub></a>
+              <a class="navbar-brand" href="{{ route('dashboard') }}">The Wolfe<sub> 0.0.1-beta</sub></a>
           </div>
           <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
